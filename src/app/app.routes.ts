750037f58b8250f4573f071890b16397b25c522e
raouane555 <rdwanalzawy6@gmail.com>
2024-07-19 09:08:38 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstPageComponent } from './view/first-page/first-page.component';
+import { SecondPageComponent } from './view/second-page/second-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/first_page', pathMatch: 'full' },
+    { path: 'first_page', component: FirstPageComponent },
+    { path: 'second_page', component: SecondPageComponent },
+    { path: '**', redirectTo: '/second_page', pathMatch: 'full' } // This is the path of 404 page change it if you want to keep it
+];
